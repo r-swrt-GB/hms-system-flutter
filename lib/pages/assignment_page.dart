01f79ac15b6ad83795a2314473401571b3ae9b68
r-swrt-GB <rikus@greenbit.dev>
2024-10-13 11:28:46 +0200

    Implemented dynamic assignment generation

--- lib/pages/assignment_page.dart
+++ lib/pages/assignment_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hms_system_application/models/assignment.dart';
+import 'package:hms_system_application/models/module.dart';
+import 'package:hms_system_application/providers/assignment_provider.dart';
+import 'package:hms_system_application/providers/module_provider.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class AssignmentPage extends StatefulWidget {
   const AssignmentPage({super.key});
@@ -9,229 +15,35 @@ class AssignmentPage extends StatefulWidget {
 
 class _AssignmentPageState extends State<AssignmentPage> {
   int _selectedModuleIndex = 0;
-  int _selectedTabIndex = 1; // Default to Assignments tab
 
   // List of modules
-  List<String> modules = [
-    "PSY101",
-    "BIO201",
-    "SOC101",
-    "COG301",
-    "PSY202",
-    "ANTH301",
-    "PSY303",
-    "ETH101",
-    "ANTH102"
-  ];
+  List<Module> modules = [];
 
-  // Assignments now have a 'module' field to associate with a specific module
-  List<Map<String, String>> assignments = [
-    {
-      "module": "PSY101",
-      "title": "Introduction to Psychology: Essay",
-      "description": "Write an essay on the impact of cognitive psychology on modern therapy.",
-      "dueDate": "Due: Sept 20, 2024"
-    },
-    {
-      "module": "BIO201",
-      "title": "Human Anatomy: Project",
-      "description": "Create a detailed project on the human circulatory system.",
-      "dueDate": "Due: Sept 25, 2024"
-    },
-    {
-      "module": "SOC101",
-      "title": "Sociology Basics: Report",
-      "description": "Submit a report on social stratification and its effects on education.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "COG301",
-      "title": "Cognitive Science: Presentation",
-      "description": "Prepare a presentation on the role of perception in cognitive processes.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY202",
-      "title": "Developmental Psychology: Case Study",
-      "description": "Analyze a case study on child development stages.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "ANTH301",
-      "title": "Biological Anthropology: Research Paper",
-      "description": "Submit a research paper on the evolutionary adaptations of humans.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "BIO201",
-      "title": "Human Anatomy: Project",
-      "description": "Create a detailed project on the human circulatory system.",
-      "dueDate": "Due: Sept 25, 2024"
-    },
-    {
-      "module": "SOC101",
-      "title": "Sociology Basics: Report",
-      "description": "Submit a report on social stratification and its effects on education.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "COG301",
-      "title": "Cognitive Science: Presentation",
-      "description": "Prepare a presentation on the role of perception in cognitive processes.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY202",
-      "title": "Developmental Psychology: Case Study",
-      "description": "Analyze a case study on child development stages.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "ANTH301",
-      "title": "Biological Anthropology: Research Paper",
-      "description": "Submit a research paper on the evolutionary adaptations of humans.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY101",
-      "title": "Introduction to Psychology: Essay",
-      "description": "Write an essay on the impact of cognitive psychology on modern therapy.",
-      "dueDate": "Due: Sept 20, 2024"
-    },
-    {
-      "module": "BIO201",
-      "title": "Human Anatomy: Project",
-      "description": "Create a detailed project on the human circulatory system.",
-      "dueDate": "Due: Sept 25, 2024"
-    },
-    {
-      "module": "SOC101",
-      "title": "Sociology Basics: Report",
-      "description": "Submit a report on social stratification and its effects on education.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "COG301",
-      "title": "Cognitive Science: Presentation",
-      "description": "Prepare a presentation on the role of perception in cognitive processes.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY202",
-      "title": "Developmental Psychology: Case Study",
-      "description": "Analyze a case study on child development stages.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "ANTH301",
-      "title": "Biological Anthropology: Research Paper",
-      "description": "Submit a research paper on the evolutionary adaptations of humans.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "BIO201",
-      "title": "Human Anatomy: Project",
-      "description": "Create a detailed project on the human circulatory system.",
-      "dueDate": "Due: Sept 25, 2024"
-    },
-    {
-      "module": "SOC101",
-      "title": "Sociology Basics: Report",
-      "description": "Submit a report on social stratification and its effects on education.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "COG301",
-      "title": "Cognitive Science: Presentation",
-      "description": "Prepare a presentation on the role of perception in cognitive processes.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY202",
-      "title": "Developmental Psychology: Case Study",
-      "description": "Analyze a case study on child development stages.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "ANTH301",
-      "title": "Biological Anthropology: Research Paper",
-      "description": "Submit a research paper on the evolutionary adaptations of humans.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY101",
-      "title": "Introduction to Psychology: Essay",
-      "description": "Write an essay on the impact of cognitive psychology on modern therapy.",
-      "dueDate": "Due: Sept 20, 2024"
-    },
-    {
-      "module": "BIO201",
-      "title": "Human Anatomy: Project",
-      "description": "Create a detailed project on the human circulatory system.",
-      "dueDate": "Due: Sept 25, 2024"
-    },
-    {
-      "module": "SOC101",
-      "title": "Sociology Basics: Report",
-      "description": "Submit a report on social stratification and its effects on education.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "COG301",
-      "title": "Cognitive Science: Presentation",
-      "description": "Prepare a presentation on the role of perception in cognitive processes.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY202",
-      "title": "Developmental Psychology: Case Study",
-      "description": "Analyze a case study on child development stages.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "ANTH301",
-      "title": "Biological Anthropology: Research Paper",
-      "description": "Submit a research paper on the evolutionary adaptations of humans.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "BIO201",
-      "title": "Human Anatomy: Project",
-      "description": "Create a detailed project on the human circulatory system.",
-      "dueDate": "Due: Sept 25, 2024"
-    },
-    {
-      "module": "SOC101",
-      "title": "Sociology Basics: Report",
-      "description": "Submit a report on social stratification and its effects on education.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "COG301",
-      "title": "Cognitive Science: Presentation",
-      "description": "Prepare a presentation on the role of perception in cognitive processes.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "PSY202",
-      "title": "Developmental Psychology: Case Study",
-      "description": "Analyze a case study on child development stages.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-    {
-      "module": "ANTH301",
-      "title": "Biological Anthropology: Research Paper",
-      "description": "Submit a research paper on the evolutionary adaptations of humans.",
-      "dueDate": "Due: Sept 30, 2024"
-    },
-  ];
+  List<Assignment> assignments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getModules();
+    getAssignments();
+  }
+
+  void getModules() async {
+    ModuleProvider moduleProvider = context.read<ModuleProvider>();
+    modules = moduleProvider.modules;
+  }
+
+  void getAssignments() async {
+    AssignmentProvider assignmentProvider = context.read<AssignmentProvider>();
+    assignments = assignmentProvider.assignments;
+  }
 
   @override
   Widget build(BuildContext context) {
     // Filter assignments based on the selected module
-    String selectedModule = modules[_selectedModuleIndex];
-    List<Map<String, String>> filteredAssignments = assignments
-        .where((assignment) => assignment['module'] == selectedModule)
+    Module selectedModule = modules[_selectedModuleIndex];
+    List<Assignment> filteredAssignments = assignments
+        .where((assignment) => assignment.moduleId == selectedModule.moduleId)
         .toList();
 
     return Scaffold(
@@ -240,7 +52,6 @@ class _AssignmentPageState extends State<AssignmentPage> {
         backgroundColor: Colors.grey,
       ),
       backgroundColor: Colors.grey,
-
       body: Column(
         children: [
           // Horizontal Scrollable Pill-shaped Module Names
@@ -255,19 +66,21 @@ class _AssignmentPageState extends State<AssignmentPage> {
                 return GestureDetector(
                   onTap: () {
                     setState(() {
-                      _selectedModuleIndex = index; // Update selected module index
+                      _selectedModuleIndex =
+                          index; // Update selected module index
                     });
                   },
                   child: Container(
                     margin: EdgeInsets.symmetric(horizontal: 8),
-                    padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 10),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 24.0, vertical: 10),
                     decoration: BoxDecoration(
                       color: isSelected ? Colors.blue : Colors.grey.shade300,
                       borderRadius: BorderRadius.circular(20),
                     ),
                     child: Center(
                       child: Text(
-                        modules[index],
+                        modules[index].moduleCode,
                         style: TextStyle(
                           color: isSelected ? Colors.white : Colors.black,
                         ),
@@ -294,85 +107,65 @@ class _AssignmentPageState extends State<AssignmentPage> {
             ),
           ),
 
-          // Scrollable List of Filtered Assignments
+          // Scrollable List of Filtered Assignments or 'No Assignments' message
           Expanded(
-            child: ListView.builder(
-              itemCount: filteredAssignments.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.all(10),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          filteredAssignments[index]["title"]!,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+            child: filteredAssignments.isNotEmpty
+                ? ListView.builder(
+                    itemCount: filteredAssignments.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: EdgeInsets.all(10),
+                        child: Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                filteredAssignments[index].assignmentTitle!,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                filteredAssignments[index]
+                                    .assignmentDescription!,
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                DateFormat('dd/MM/yyyy at HH:mm')
+                                    .format(filteredAssignments[index].dueDate),
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        SizedBox(height: 5),
+                      );
+                    },
+                  )
+                : Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
                         Text(
-                          filteredAssignments[index]["description"]!,
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          filteredAssignments[index]["dueDate"]!,
+                          "No assignments 😊",
                           style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade700,
                           ),
                         ),
                       ],
                     ),
                   ),
-                );
-              },
-            ),
           ),
         ],
-      ),
-
-      // Bottom Navigation Bar with grey background and blue icons
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.grey,  // Set the background color of the bar to grey
-        selectedItemColor: Colors.blue, // Set selected icon color to blue
-        unselectedItemColor: Colors.blue, // Set unselected icon color to blue
-        
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.assignment),
-            label: 'Assignments',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedTabIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedTabIndex = index; // Update selected tab index
-          });
-        },
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: AssignmentPage(),
-  ));
 }
